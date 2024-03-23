4b638028b95e31125c298ee046af441706d0c08d
Thomas Booij <thomas@booijanalytics.nl>
2024-03-23 13:10:42 +0000

    add billing app

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,6 +82,8 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "booij_analytics.users",
+    "booij_analytics.billing",
+    
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
